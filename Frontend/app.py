0ba517e6d78405aeb3ad46a1bfbfe250ef31b40f
Owais Hetavkar <50790949+owaisjh@users.noreply.github.com>
2023-04-08 17:23:46 -0700

    integration updates

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,24 +2,27 @@ import os
 import glob
 import streamlit as st
 from pathlib import Path
+import pandas as pd
 import nltk
 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
 
 from drivers.txtToTags import txtToTags
 from drivers.imgToTags import imgToTags
+from drivers.findSimilarImgs import findSimilarImgs
 
 
 
 UPLOAD_DIR = 'temp'  # Directory where uploaded images will be saved
 IMAGE_EXTENSIONS = ['png', 'jpg']  # List of accepted image file extensions
-DISPLAY_DIR = 'Frontend/accept'  # Directory where displayed images will be saved
+DISPLAY_DIR = 'data/flickr30k-images'  # Directory where displayed images will be saved
 DISPLAY_COLUMNS = 5  # Number of columns in which to display images
 
 # Function to display saved images in columns
-def display_images():
-    view_images = glob.glob(f'{DISPLAY_DIR}/*.jpg')
+def display_images(imagesList):
+    view_images = [os.path.join(DISPLAY_DIR, img) for img in imagesList]
+
     groups = [view_images[i:i+DISPLAY_COLUMNS] for i in range(0, len(view_images), DISPLAY_COLUMNS)]
     cols = st.columns(DISPLAY_COLUMNS)
     for group in groups:
@@ -31,10 +34,6 @@ with st.sidebar:
     st.title("Image caption generator")
     choice = st.radio("Navigation", ["Text", "Image"])
     st.info("This is an application that can be used to generate images using text or other images as input")
-
-
-
-
 
 
 
@@ -52,28 +51,27 @@ if choice == "Image":
                 with open(save_path, mode='wb') as w:
                     w.write(file.getvalue())
                 if save_path.exists():
-                    tags = imgToTags(file.name)
+                    tags = imgToTags(save_path)
                     os.remove(save_path)
-                    print(tags)
-                    st.success(f'File {file.name} is successfully saved!')
+                    st.success(tags)
+                    df = findSimilarImgs(tags, threshold = 0.8)
+                    st.write(df)
+
+                    display_images(df['Image file name'])
+
+                    
             except Exception as e:
                 st.error(f'Error saving file: {e}')
 
     # When Display button is clicked, save uploaded images to DISPLAY_DIR and display them
-    if st.button('Display'):
-        try:
-            os.makedirs(DISPLAY_DIR, exist_ok=True)
-            for file in glob.glob(f'{UPLOAD_DIR}/*.*'):
-                if file.endswith(tuple(IMAGE_EXTENSIONS)):
-                    save_path = Path(DISPLAY_DIR, os.path.basename(file))
-                    with open(save_path, mode='wb') as w:
-                        w.write(open(file, mode='rb').read())
-            display_images()
-        except Exception as e:
-            st.error(f'Error displaying images: {e}')
+    # if st.button('Display'):
+    #     try:
+    #         print('hello')
+    #         display_images()
 
 
-
+    #     except Exception as e:
+    #         st.error(f'Error displaying images: {e}')
 
 
 
@@ -81,9 +79,18 @@ if choice == "Image":
 if choice == "Text":
     st.title("Insert Your Text here!")
     text_input = st.text_input("Enter some text 👇")
+    
     if st.button('Submit'):
         if text_input:
             tags = txtToTags(text_input)
-            st.success(tags)
-            if st.button('Display Similar Images?'):
-                print("due")
+            st.write(tags)
+
+            df = findSimilarImgs(tags, threshold = 0.8)
+            st.write(df)
+
+            display_images(df['Image file name'])
+
+
+
+                
+                
